12d9075010cc9ed73dfcce10f5fc32eea398d86e
Apoorva Muralidhara <apoorva_muralidhara@yahoo.com>
2019-06-05 04:53:10 -0400

    Added the last piece of the happy path of CSV import, joining users to skills.

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,17 +7,17 @@ class User < ApplicationRecord
   def self.import(path)
     CSV.foreach(path, headers: true) do |row|
       csv_values = row.to_hash
-      User.create!(email: csv_values['email'],
-                   name: csv_values['name'],
-                   global_admin: (csv_values[:'global_admin'] == 't'),
-                   timezone: csv_values['_timezone'],
-                   receive_marketing: (csv_values['receive_marketing'] == 't'),
-                   external_identifier: csv_values['external_id'])
+      user = User.create!(email: csv_values['email'],
+                          name: csv_values['name'],
+                          global_admin: (csv_values[:'global_admin'] == 't'),
+                          timezone: csv_values['_timezone'],
+                          receive_marketing: (csv_values['receive_marketing'] == 't'),
+                          external_identifier: csv_values['external_id'])
 
       next unless csv_values['Skills'].present?
       
       csv_values['Skills'].split(',').each do |name|
-        Skill.find_or_create_by!(name: name.strip)
+        user.skills << Skill.find_or_create_by!(name: name.strip)
       end
      end
   end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -26,5 +26,21 @@ RSpec.describe User, type: :model do
       expect(Skill.pluck(:name))
         .to match_array(%w(Dancing Prancing Chancing))
     end
+
+    it 'matches users to skills' do
+      expect(User.find_by!(email: 'scott.baker@ux.io').skills.pluck(:name))
+        .to match_array(%w(Dancing Prancing))
+      expect(User.find_by!(email: 'cathy.carey@ux.io').skills.pluck(:name))
+        .to contain_exactly('Chancing')
+      expect(User.find_by!(email: 'lynn.yates@ux.io').skills.pluck(:name))
+        .to contain_exactly('Prancing')
+
+      expect(Skill.find_by!(name: 'Dancing').users.pluck(:email))
+        .to contain_exactly('scott.baker@ux.io')
+      expect(Skill.find_by!(name: 'Chancing').users.pluck(:email))
+        .to contain_exactly('cathy.carey@ux.io')
+      expect(Skill.find_by!(name: 'Prancing').users.pluck(:email))
+        .to contain_exactly('scott.baker@ux.io', 'lynn.yates@ux.io')
+    end
   end
 end
