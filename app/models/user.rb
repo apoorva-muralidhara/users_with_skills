09efdd6a426c66ca9f52145bc2e76e9a5ba75bdc
Apoorva Muralidhara <apoorva_muralidhara@yahoo.com>
2019-06-05 02:35:36 -0400

    Added similar associations on User.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  has_many :user_skills, dependent: :destroy
+  has_many :skills, through: :user_skills
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { is_expected.to have_many(:user_skills).dependent(:destroy) }
+    it { is_expected.to have_many(:skills).through(:user_skills) }
+  end
 end
