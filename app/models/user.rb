a854be89c3336d808096f7b33ad45c584b04c1ab
Apoorva Muralidhara <apoorva_muralidhara@yahoo.com>
2019-06-05 04:24:56 -0400

    Implemented a minimal happy-path User.import.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,14 @@ class User < ApplicationRecord
   has_many :skills, through: :user_skills
 
   def self.import(path)
-    # CSV.foreach(path, headers: true) do |row|
-    #   puts "It's #{row.to_hash.inspect}!"
-    # end
+    CSV.foreach(path, headers: true) do |row|
+      csv_values = row.to_hash
+      User.create!(email: csv_values['email'],
+                   name: csv_values['name'],
+                   global_admin: (csv_values[:'global_admin'] == 't'),
+                   timezone: csv_values['_timezone'],
+                   receive_marketing: (csv_values['receive_marketing'] == 't'),
+                   external_identifier: csv_values['external_id'])
+     end
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,4 +5,20 @@ RSpec.describe User, type: :model do
     it { is_expected.to have_many(:user_skills).dependent(:destroy) }
     it { is_expected.to have_many(:skills).through(:user_skills) }
   end
+
+  describe '.import' do
+    let(:path) { file_fixture('three_users.csv') }
+
+    it 'creates users' do
+      User.import(path)
+
+      expect(User.count).to eq(3)
+      expect(User.find_by!(email: 'scott.baker@ux.io'))
+        .to have_attributes(name: 'Scott Baker',
+                            global_admin: false,
+                            timezone: 'PST',
+                            receive_marketing: false,
+                            external_identifier: 's7754756893610')
+    end
+  end
 end
