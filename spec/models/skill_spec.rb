e738c5a56867bdc857fdcbbef345197404ea0903
Apoorva Muralidhara <apoorva_muralidhara@yahoo.com>
2019-06-05 02:28:36 -0400

    Configured FactoryBot and added Skill name validations.

--- spec/models/skill_spec.rb
+++ spec/models/skill_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Skill, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:name) { 'unicycling' }
+
+  subject(:skill) { create(:skill, name: name) }
+  # describe 'associations' do
+  #   it { is_expected.to have_many(:user_skills).dependent(:destroy) }
+  # end
+
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_uniqueness_of(:name) }
+  end
 end
